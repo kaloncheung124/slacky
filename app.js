9cd4f184da2aa6f8cdae5061cb33d6b4d763bdf3
Kalon Cheung <kalon@berkeley.edu>
2018-10-30 23:20:01 +0000

    fixing bugs lol

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ function buildResponse(req, res, next) {
         req.slackPost = validResponses[Math.floor(Math.random()*validResponses.length)];
     }
     
-    if (specialResponses.keys.includes.indexOf(req.slackPost) !== -1) {
+    if (Object.values(specialResponses)includes(req.slackPost)) {
         return respondSpecially(req, reqMessage, next);
     } else {
         next();
