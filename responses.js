8c40e5fcb98c3f36ebd571e598a4ad22ca72166e
Kalon Cheung <kalon.c@taboola.com>
2019-06-12 14:24:51 -0700

    making a diff secret yes

--- responses.js
+++ responses.js
@@ -40,7 +40,7 @@ const custom_messages = [
     triggers: ["is", "are", "does", "do", "will", "should", "could", "am", "would", "did", "were", "can"],
     responses: [":no_good: :no_entry_sign: no :no_good: :no_entry_sign:", ":the_horns::dancing-party: yes:the_horns::dancing-party:"]
   }, {
-    triggers: ["​is", "​are", "​does", "​do", "​will", "​should", "​could", "​am", "​would", "​did", "​were", "​can"],
+    triggers: ["...is", "...are", "...does", "...do", "...will", "...should", "...could", "...am", "...would", "...did", "...were", "...can"],
     responses: [":the_horns::dancing-party: yes:the_horns::dancing-party:"]
   }, {
     triggers: ['do you like'],
