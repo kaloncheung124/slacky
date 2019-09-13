b9d78804b307b1baa274dee458e217fa51925381
Kalon Cheung <kalon.c@taboola.com>
2019-09-13 15:59:29 -0700

    fixing indentation and adding what is

--- app.js
+++ app.js
@@ -16,88 +16,88 @@ app.use(bodyParser.json());
 // NEXT STEPS:
 
 function buildResponse(req, res, next) {
-    console.log(req.body);
-    let body = req.body;
-    
-    // Ensure correct command and slack team invoked this.
-    if (body.command !== app_command || body.token !== app_token) {
-        next("Incorrect command or team.")
-        return;
+  console.log(req.body);
+  let body = req.body;
+
+  // Ensure correct command and slack team invoked this.
+  if (body.command !== app_command || body.token !== app_token) {
+    next("Incorrect command or team.")
+    return;
+  }
+
+  // If it's not in the correct channel, don't reply
+  if (app_channel_ids.indexOf(body.channel_id) === -1) {
+    next("Slacky doesn't work here.");
+    return;
+  }
+
+  // Otherwise, search for valid response
+  let reqMessage = body.text.toLowerCase();
+  let lowestSubstringIndex = Number.POSITIVE_INFINITY;
+  let longestTriggerLength = Number.NEGATIVE_INFINITY;
+  let bestResponse;
+  req.shouldPost = false;
+
+  // Using the explicit for loops for performance, iterate through each trigger of each customResponse
+  for (let i = 0; i < customResponses.length; i++) {
+    let triggers = customResponses[i].triggers;
+
+    for (let j = 0; j < triggers.length; j++) {
+      let trigger = triggers[j];
+
+      // If trigger not in message, continue to next trigger.
+      let substringIndex = reqMessage.indexOf(trigger);
+      if (substringIndex === -1) continue;
+
+      let triggerLength = trigger.length;
+
+      // If this trigger's index is lower, or it's the same and the trigger string length is greater, choose this response index.
+      if (substringIndex < lowestSubstringIndex || (substringIndex === lowestSubstringIndex && triggerLength > longestTriggerLength)) {
+
+        lowestSubstringIndex = substringIndex;
+        longestTriggerLength = triggerLength;
+        bestResponse = i;
+        req.shouldPost = true;
+      }
     }
-    
-    // If it's not in the correct channel, don't reply
-    if (app_channel_ids.indexOf(body.channel_id) === -1) {
-        next("Slacky doesn't work here.");
-        return;
-    }
-    
-    // Otherwise, search for valid response
-    let reqMessage = body.text.toLowerCase();
-    let lowestSubstringIndex = Number.POSITIVE_INFINITY;
-    let longestTriggerLength = Number.NEGATIVE_INFINITY;
-    let bestResponse;
-    req.shouldPost = false;
-    
-    // Using the explicit for loops for performance, iterate through each trigger of each customResponse
-    for (let i = 0; i < customResponses.length; i++) {
-        let triggers = customResponses[i].triggers;
-        
-        for (let j = 0; j < triggers.length; j++) {
-            let trigger = triggers[j];
-            
-            // If trigger not in message, continue to next trigger.
-            let substringIndex = reqMessage.indexOf(trigger);
-            if (substringIndex === -1) continue;
-            
-            let triggerLength = trigger.length;
-            
-            // If this trigger's index is lower, or it's the same and the trigger string length is greater, choose this response index.
-            if (substringIndex < lowestSubstringIndex || (substringIndex === lowestSubstringIndex && triggerLength > longestTriggerLength)) {
-                    
-                lowestSubstringIndex = substringIndex;
-                longestTriggerLength = triggerLength;
-                bestResponse = i;
-                req.shouldPost = true;
-            }
-        }
-    }
-    
-    // If valid response, pick a random response
-    if (req.shouldPost) {
-        let validResponses = customResponses[bestResponse].responses;
-        req.slackPost = validResponses[Math.floor(Math.random()*validResponses.length)];
-    }
-    
-    if (Object.values(specialResponses).includes(req.slackPost)) {
-        return respondSpecially(req, reqMessage, next);
-    } else {
-        next();
-    }
+  }
+
+  // If valid response, pick a random response
+  if (req.shouldPost) {
+    let validResponses = customResponses[bestResponse].responses;
+    req.slackPost = validResponses[Math.floor(Math.random()*validResponses.length)];
+  }
+
+  if (Object.values(specialResponses).includes(req.slackPost)) {
+    return respondSpecially(req, reqMessage, next);
+  } else {
+    next();
+  }
 }
 
 function confirmOK(req, res) {
-    if (req.shouldPost) {
-        console.log("Response:", req.slackPost);
-        res.status(200).json({
-            "response_type": "in_channel",
-            "link_names": 1,
-            "text": req.slackPost
-        });
-    } else {
-        console.log("No response found.")
-        res.status(200).send("No :fire: response found.");
-    }
+  if (req.shouldPost) {
+    console.log("Response:", req.slackPost);
+    res.status(200).json({
+      "response_type": "in_channel",
+      "link_names": 1,
+      "text": req.slackPost
+    });
+  } else {
+    console.log("No response found.")
+    res.status(200).send("No :fire: response found.");
+  }
 }
 
 function errorHandler(err, req, res, next) {
-    console.error(err);
-    res.status(200).send(err);
+  console.error(err);
+  res.status(200).send(err);
 }
 
 app.post('/slacky',
-    buildResponse,
-    confirmOK,
-    errorHandler
+  buildResponse,
+  confirmOK,
+  errorHandler
 )
 
 app.get('/', function(req, res) {
--- specialResponses.js
+++ specialResponses.js
@@ -1,109 +1,115 @@
 const Axios = require('axios');
 
 const specialResponses = {
-    BITCOIN_LOOKUP: "SP_BITCOIN_LOOKUP",
-    LOVE_BACK: "SP_LOVE_BACK",
-    CIRCLE_TEXT: "SP_CIRCLE_TEXT",
-    TEXT: "SP_TEXT",
+  BITCOIN_LOOKUP: "SP_BITCOIN_LOOKUP",
+  LOVE_BACK: "SP_LOVE_BACK",
+  CIRCLE_TEXT: "SP_CIRCLE_TEXT",
+  TEXT: "SP_TEXT",
+  WHAT_IS: "WHAT_IS",
 };
 
 module.exports.specialResponses = specialResponses;
 
 // Must call next!
 module.exports.respondSpecially = function(req, reqMessage, next) {
-    switch (req.slackPost) {
-        case specialResponses.BITCOIN_LOOKUP:
-            return Axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-                .then(function(response) {
-                    req.slackPost = "*1 BTC = " + response.data.bpi.USD.rate + " USD* as of " + response.data.time.updated;
-                    next();
-                });
-        case specialResponses.LOVE_BACK:
-            req.slackPost = `I love you too, <@${req.body.user_id}>`;
-            return next();
-            
-        case specialResponses.CIRCLE_TEXT:
-            // ASSUMES the text starts after the first word
-            let textToCirclify = reqMessage.substr(reqMessage.indexOf(" ") + 1);
-            req.slackPost = textIntoCircles(textToCirclify);
-            return next();
+  switch (req.slackPost) {
+    case specialResponses.BITCOIN_LOOKUP:
+      return Axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        .then(function(response) {
+          req.slackPost = "*1 BTC = " + response.data.bpi.USD.rate + " USD* as of " + response.data.time.updated;
+          next();
+        });
+    case specialResponses.LOVE_BACK:
+      req.slackPost = `I love you too, <@${req.body.user_id}>`;
+      return next();
 
-        case specialResponses.TEXT:
-          // ASSUMES the text starts after the first word
-          let textToTranslate = reqMessage.substr(reqMessage.indexOf(" ") + 1);
-          req.slackPost = textToEmoji(textToTranslate);
-          return next();
-        default:
-            return next();
-    }
+    case specialResponses.CIRCLE_TEXT:
+      // ASSUMES the text starts after the first word
+      let textToCirclify = reqMessage.substr(reqMessage.indexOf(" ") + 1);
+      req.slackPost = textIntoCircles(textToCirclify);
+      return next();
+
+    case specialResponses.TEXT:
+      // ASSUMES the text starts after the first word
+      let textToTranslate = reqMessage.substr(reqMessage.indexOf(" ") + 1);
+      req.slackPost = textToEmoji(textToTranslate);
+      return next();
+    case specialResponses.WHAT_IS:
+      // Assumes the message starts after
+      let textToSearch = reqMessage.toLowerCase();
+      req.slackPost = `https://www.google.com/search?q=${textToSearch}`;
+      return next();
+    default:
+      return next();
+  }
 };
 
 const englishTextRegex = /^[a-z]$/i;
 function getTextIconString(char) {
-    // If it's a space, double it to space out the big words.
-    if (char === " ")
-        return "  ";
+  // If it's a space, double it to space out the big words.
+  if (char === " ")
+    return "  ";
 
-    let validChar = englishTextRegex.test(char);
-    return validChar ? `:${char.toLowerCase()}-lowercase:` : char;
+  let validChar = englishTextRegex.test(char);
+  return validChar ? `:${char.toLowerCase()}-lowercase:` : char;
 }
 
 function textToEmoji(text) {
-    let output = "";
-    for (let char of text) {
-        output += getTextIconString(char);
-    }
+  let output = "";
+  for (let char of text) {
+    output += getTextIconString(char);
+  }
 
-    return output;
+  return output;
 }
 
 const letterMap = {
-    a: "atrain",
-    b: "btrain",
-    c: "ctrain",
-    d: "dtrain",
-    e: "etrain",
-    f: "ftrain",
-    g: "gtrain",
-    h: "htrain",
-    i: "i",
-    j: "jtrain",
-    k: "ktrain",
-    l: "ltrain",
-    m: "mtrain",
-    n: "ntrain",
-    o: "target",
-    p: "ptrain",
-    q: "qtrain",
-    r: "rtrain",
-    s: "strain",
-    t: "ttrain",
-    u: "utrain",
-    v: "vtrain",
-    w: "wtrain",
-    x: "xcircle",
-    y: "y",
-    z: "ztrain",
-    "?": "question",
-    "0": "0train",
-    "1": "1train",
-    "2": "2train",
-    "3": "3train",
-    "4": "4train",
-    "5": "5train",
-    "6": "6train",
-    "7": "7train",
-    "8": "8train",
-    "9": "9train"
+  a: "atrain",
+  b: "btrain",
+  c: "ctrain",
+  d: "dtrain",
+  e: "etrain",
+  f: "ftrain",
+  g: "gtrain",
+  h: "htrain",
+  i: "i",
+  j: "jtrain",
+  k: "ktrain",
+  l: "ltrain",
+  m: "mtrain",
+  n: "ntrain",
+  o: "target",
+  p: "ptrain",
+  q: "qtrain",
+  r: "rtrain",
+  s: "strain",
+  t: "ttrain",
+  u: "utrain",
+  v: "vtrain",
+  w: "wtrain",
+  x: "xcircle",
+  y: "y",
+  z: "ztrain",
+  "?": "question",
+  "0": "0train",
+  "1": "1train",
+  "2": "2train",
+  "3": "3train",
+  "4": "4train",
+  "5": "5train",
+  "6": "6train",
+  "7": "7train",
+  "8": "8train",
+  "9": "9train"
 };
 
 function textIntoCircles(str) {
-    let output = "";
-    for (let char of str) {
-        let iconName = letterMap[char.toLowerCase()];
-        let iconText = iconName ? `:${iconName}:` : char;
-        output += char === " " ? "  " : iconText;
-    }
+  let output = "";
+  for (let char of str) {
+    let iconName = letterMap[char.toLowerCase()];
+    let iconText = iconName ? `:${iconName}:` : char;
+    output += char === " " ? "  " : iconText;
+  }
 
-    return output;
+  return output;
 }
